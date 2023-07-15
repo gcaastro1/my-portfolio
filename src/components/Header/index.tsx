0e3a6608c2b12e68b5a3f8020e68e5717fb111bc
gustavo <gcaastro1@gmail.com>
2023-07-15 16:17:00 -0300

    feat: add header & avatar/title animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,6 +7,8 @@ import styles from "./style.module.scss"
 import { motion } from 'framer-motion'
 import { useContext } from 'react'
 import { ThemeContext } from '@/contexts/ThemeContext'
+import Typing from './Typing'
+import { ArrowDown } from '@phosphor-icons/react'
 
 const spinTransition = {
     repeat: Infinity,
@@ -27,6 +29,11 @@ const Header = () => {
                 />
                 <Image src={mypic} alt={'Picture of author'} priority={true}/>
             </div>
+            <h1 className={`${styles.header__title} text`}>Hello, I&apos;m a</h1>
+            <Typing />
+            <button className={`${styles.header__button} buttonbg`}>
+                Discover my works <ArrowDown size={16} />
+            </button>
         </div>
     )
 }
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ import Logo from "./Logo"
 const Navbar = () => {
   
     return (
-        <div className={`${styles.navbar} background`}>
+        <div className={`${styles.navbar} background border__bottom`}>
             <div className={styles.navbar__content}>
                 <Logo />
                 <NavButtons />
